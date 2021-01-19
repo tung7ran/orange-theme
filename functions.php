6f1d51ee44c43b2c8a5e0f44d4f667bfef5726a3
tung7ran <tranthanhtung52@gmail.com>
2021-01-20 00:27:01 +0700

    update name _s in all template

--- functions.php
+++ functions.php
@@ -7,9 +7,9 @@
  * @package orange-theme
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if ( ! defined( '_ORANGE_THEME_VERSION' ) ) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define( '_ORANGE_THEME_VERSION', '1.0.0' );
 }
 
 if ( ! function_exists( 'orange_theme_setup' ) ) :
@@ -140,10 +140,10 @@ add_action( 'widgets_init', 'orange_theme_widgets_init' );
  * Enqueue scripts and styles.
  */
 function orange_theme_scripts() {
-	wp_enqueue_style( 'orange-theme-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_enqueue_style( 'orange-theme-style', get_stylesheet_uri(), array(), _ORANGE_THEME_VERSION );
 	wp_style_add_data( 'orange-theme-style', 'rtl', 'replace' );
 
-	wp_enqueue_script( 'orange-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'orange-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _ORANGE_THEME_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
